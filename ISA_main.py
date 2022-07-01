e894d48a319a21a03ef53a41025f24b84a1a9b76
SachinSharma-IIITD <sachin21559@iiitd.ac.in>
2022-07-01 18:47:47 +0530

    modified main flow

--- ISA_main.py
+++ ISA_main.py
@@ -1,71 +1,85 @@
-from msilib.schema import Error
 import sys
 from error import Symbolerror
 from FinalAboveMain import opcode
 
-prog = [i.strip() for i in sys.stdin.read().split('\n')]
 
-vars = dict()
-labels = dict()
-instructions = []
+def main():
+    prog = [i.strip() for i in sys.stdin.read().split('\n')]
 
-is_var = 1
+    vars = dict()
+    labels = dict()
+    instructions = []
 
-counter = 0
+    is_var = 1
 
-for line in prog:
-    Symbolerror(line)
+    counter = 0
 
-    line = line.split(' ')
+    for line in prog:
+        Symbolerror(line)
 
-    # Var check
-    if line[0] == 'var':
-        if is_var == 0:
-            raise(Error)
-        else:
-            if len(line) == 2 and line[1].isalnum():       # Var check
-                var_name = line[1]
-                
-                if var_name in vars:
-                    raise(Error)    # Redefine vars
-
-                vars[line[1]] = 0
-                continue
-            else:
-                raise(Error)
-
-    # Label check
-    if ':' in line:
-        if ':' not in line[0] or ':' != line[0][-1]:
-            raise(Error)    # : at wrong pos
-        if ':' in line[1:]:
-            raise(Error)    # label redefined
-        if len(line[0]) <= 1:
-            raise(Error)    # empty label
-
-        label_name = line[0][:-1]
-        if not label_name.isalnum():
-            raise(Error)    # Invalid label name
+        line = line.split(' ')
+        # print(line)
         
-        if label_name in labels:
-            raise(Error)    # redefine lables
-            
-        labels[label_name] = counter
+        # Var check
+        if line[0] == 'var':
+            if is_var == 0:
+                print('6')
+                exit()
+            else:
+                if len(line) == 2 and line[1].isalnum():       # Var check
+                    var_name = line[1]
+
+                    if var_name in vars:
+                        print('10')
+                        exit()    # Redefine vars
+
+                    vars[line[1]] = 0
+                    continue
+                else:
+                    print('general')
+                    exit()
+
+        # Label check
+        if ':' in " ".join(line):
+            if ':' not in line[0] or ':' != line[0][-1]:
+                print('17')
+                exit()    # : at wrong pos
+            if ':' in ' '.join(line[1:]):
+                print('9')
+                exit()    # label redefined
+            if len(line[0]) <= 1:
+                print('8')
+                exit()    # empty label
+
+            label_name = line[0][:-1]
+            if not label_name.isalnum():
+                print('8')
+                exit()    # Invalid label name
+
+            if label_name in labels:
+                print('9')
+                exit()    # redefine lables
+
+            labels[label_name] = counter
+            counter += 1
+            instructions.append(line[1:])
+            continue
+
+        # Blank Line
+        if line == '\n':
+            continue
+
+        # Invalid instr
+        # if line[0] not in opcode:
+        #     print('0')
+        #     exit()
+
+        instructions.append(line)
         counter += 1
-        instructions.append(line[1:])
-        continue
 
-    # Blank Line
-    if line == '\n':
-        continue
-    
-    # Invalid instr
-    if line[0] not in opcode:
-        raise(Error)
-    
-    instructions.append(line)
-    counter += 1
+    for v in vars:
+        vars[v] = counter
+        counter += 1
 
-for v in vars:
-    vars[v] = counter
-    counter += 1
+
+main()
